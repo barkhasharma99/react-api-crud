3118b389909874e698f7935cd18b6f6cac74e6a3
barkha sharma <barkha.sharma55722@gmail.com>
2022-05-19 15:37:21 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./components/pages/Home";
+import View from "./components/student/View";
+ 
+ 
+import Edit from "./components/student/Edit";
+
+ 
+function App() {
+  return (
+    <>
+      
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/view/:id" component={View} />
+      
+        
+       
+         
+          <Route  path="/edit/:id"  component={Edit}/>
+        </Switch>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
